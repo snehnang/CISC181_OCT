4e3dc6056684856cb2ed7eebbaeca34e02b35c3a
sneha <snehnang@udel.edu>
2023-11-14 16:48:22 -0500

    GradeController, GradeDTO

---
+++ Server/Controllers/UD/GradeController.cs
@@ -0,0 +1,113 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using OCTOBER.EF.Data;
+using OCTOBER.Server.Controllers.Base;
+using OCTOBER.Shared.DTO;
+
+namespace OCTOBER.Server.Controllers.UD
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class GradeController : BaseController, GenericRestController<GradeDTO>
+    {
+        public GradeController(OCTOBEROracleContext context,
+            IHttpContextAccessor httpContextAccessor,
+            IMemoryCache memoryCache)
+        : base(context, httpContextAccessor)
+        {
+        }
+
+        [HttpDelete]
+        [Route("Delete/{KeyVal}")]
+        public Task<IActionResult> Delete(int KeyVal)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var result = await _context.Grades.Select(sp => new GradeDTO
+                {
+                    Comments = sp.Comments,
+                    CreatedBy = sp.CreatedBy,
+                    CreatedDate = sp.CreatedDate,
+                    GradeCodeOccurrence = sp.GradeCodeOccurrence,
+                    GradeTypeCode = sp.GradeTypeCode,
+                    ModifiedBy = sp.ModifiedBy,
+                    ModifiedDate = sp.ModifiedDate,
+                    NumericGrade = sp.NumericGrade,
+                    SchoolId = sp.SchoolId,
+                    SectionId = sp.SectionId,
+                    StudentId = sp.StudentId
+                })
+                .ToListAsync();
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        [HttpGet]
+        [Route("Get/{KeyVal}")]
+        public async Task<IActionResult> Get(int KeyVal)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                GradeDTO? result = await _context.Grades
+                    .Where(x => x.SchoolId == KeyVal)
+                    .Select(sp => new GradeDTO
+                {
+                    Comments = sp.Comments,
+                    CreatedBy = sp.CreatedBy,
+                    CreatedDate = sp.CreatedDate,
+                    GradeCodeOccurrence = sp.GradeCodeOccurrence,
+                    GradeTypeCode = sp.GradeTypeCode,
+                    ModifiedBy = sp.ModifiedBy,
+                    ModifiedDate = sp.ModifiedDate,
+                    NumericGrade = sp.NumericGrade,
+                    SchoolId = sp.SchoolId,
+                    SectionId = sp.SectionId,
+                    StudentId = sp.StudentId
+                })
+                .SingleAsync();
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        [HttpPost]
+        [Route("Post")]
+        public Task<IActionResult> Post([FromBody] GradeDTO _T)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        [Route("Put")]
+        public Task<IActionResult> Put([FromBody] GradeDTO _T)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
