78c032aacfa5f3dbb794b0d2f220576d1d67389f
sneha <snehnang@udel.edu>
2023-11-14 18:17:59 -0500

    ZipcodeController, ZipcodeDTO

---
+++ Server/Controllers/UD/ZipcodeController.cs
@@ -0,0 +1,105 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
+using OCTOBER.EF.Data;
+using OCTOBER.Server.Controllers.Base;
+using OCTOBER.Shared.DTO;
+
+namespace OCTOBER.Server.Controllers.UD
+{
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class ZipcodeController : BaseController, GenericRestController<ZipcodeDTO>
+    {
+        public ZipcodeController(OCTOBEROracleContext context,
+            IHttpContextAccessor httpContextAccessor,
+            IMemoryCache memoryCache)
+        : base(context, httpContextAccessor)
+        {
+        }
+
+        [HttpDelete]
+        [Route("Delete/{KeyVal}")]
+        public Task<IActionResult> Delete(int KeyVal)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpGet]
+        [Route("Get")]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                var result = await _context.Zipcodes.Select(sp => new ZipcodeDTO
+                {
+                    City = sp.City,
+                    CreatedBy = sp.CreatedBy,
+                    CreatedDate = sp.CreatedDate,
+                    ModifiedBy = sp.ModifiedBy,
+                    ModifiedDate = sp.ModifiedDate,
+                    State = sp.State,
+                    Zip = sp.Zip
+                })
+                .ToListAsync();
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        [HttpGet]
+        [Route("Get/{KeyVal}")]
+        public async Task<IActionResult> Get(string KeyVal)
+        {
+            try
+            {
+                await _context.Database.BeginTransactionAsync();
+
+                ZipcodeDTO? result = await _context.Zipcodes
+                    .Where(x=>x.Zip == KeyVal)
+                    .Select(sp => new ZipcodeDTO
+                {
+                    City = sp.City,
+                    CreatedBy = sp.CreatedBy,
+                    CreatedDate = sp.CreatedDate,
+                    ModifiedBy = sp.ModifiedBy,
+                    ModifiedDate = sp.ModifiedDate,
+                    State = sp.State,
+                    Zip = sp.Zip
+                })
+                .SingleAsync();
+                await _context.Database.RollbackTransactionAsync();
+                return Ok(result);
+            }
+            catch (Exception Dex)
+            {
+                await _context.Database.RollbackTransactionAsync();
+                //List<OraError> DBErrors = ErrorHandling.TryDecodeDbUpdateException(Dex, _OraTranslateMsgs);
+                return StatusCode(StatusCodes.Status417ExpectationFailed, "An Error has occurred");
+            }
+        }
+
+        [HttpPost]
+        [Route("Post")]
+        public Task<IActionResult> Post([FromBody] ZipcodeDTO _T)
+        {
+            throw new NotImplementedException();
+        }
+
+        [HttpPut]
+        [Route("Put")]
+        public Task<IActionResult> Put([FromBody] ZipcodeDTO _T)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
